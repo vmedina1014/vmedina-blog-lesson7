7adb9e05fee40a8a9d4b2073ef41fd4bb3d1a155
Victor Medina <vmedina1014@gmail.com>
2020-01-15 20:07:54 -0800

    lesson 8 modified files, added allauth and Modelform

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,23 @@
+
 from django.contrib import admin
-from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+from blogging.models import Post, Category, ModelAdmin2
 
+
+class ModelAdminClass(admin.TabularInline):
+    model = ModelAdmin2
+    extra = 1
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = (ModelAdminClass,)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+# and a new admin registration
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+# Register your models here.
